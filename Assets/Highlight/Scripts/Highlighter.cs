3323625c2305876dc2b0a46af0699675d79fc974
cblech <cblech@ymail.com>
2021-11-22 19:18:34 +0100

    Add Selection outline

--- Assets/Highlight/Scripts/Highlighter.cs
+++ Assets/Highlight/Scripts/Highlighter.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using JetBrains.Annotations;
 
 
 public class Highlighter : Interface3DHover
@@ -12,46 +14,93 @@ public class Highlighter : Interface3DHover
     public const int UVChannel = 3;
 
     [SerializeField]
-    private Highlight highlightPrefab = default;
+    private Highlight _hoverHighlightPrefab = default;
+
+    [SerializeField]
+    private Highlight _selectedHighlightPrefab = default;
 
 
-    private bool highlightEnabled = false;
-    private Highlight[] highlights;
+    private bool _isHovering = false;
+    private Highlight[] activeHighlights = Array.Empty<Highlight>();
 
+    private Entity OwnEntity => GetComponent<Entity>();
+
+    void Start()
+    {
+        SceneManager.OnUpdateSelection.AddListener(SetDirty);
+    }
+
+    private bool _isDirty = false;
+
+    public void SetDirty()
+    {
+        _isDirty = true;
+    }
+
+    void LateUpdate()
+    {
+        if (_isDirty)
+        {
+            _isDirty = false;
+            UpdateHovering();
+        }
+    }
+
+    public void UpdateHovering()
+    {
+        if (_isHovering)
+        {
+            SetHighlight(_hoverHighlightPrefab);
+        }
+        else if (OwnEntity == SceneManager.SelectedEntity)
+        {
+            SetHighlight(_selectedHighlightPrefab);
+        }
+        else
+        {
+            SetHighlight(null);
+        }
+    }
 
     public override void StartHover()
     {
-        if (!highlightEnabled)
-        {
-            Hilightable[] targets = GetComponentsInChildren<Hilightable>();
-            highlights = new Highlight[targets.Length];
-            InstantiateHighlights(targets);
-
-            highlightEnabled = true;
-        }
+        _isHovering = true;
+        UpdateHovering();
     }
-
-
+    
     public override void EndHover()
     {
-        if (highlightEnabled)
-        {
-            highlightEnabled = false;
-
-            foreach (Highlight highlight in highlights)
-            {
-                highlight.DestroyHighlight();
-            }
-            
-        }
+        _isHovering = false;
+        UpdateHovering();
     }
 
-
-    private void InstantiateHighlights(Hilightable[] targets)
+    private void SetHighlight([CanBeNull] Highlight highlight)
     {
-        for (int i = 0; i < targets.Length; i++)
+        Debug.Log(highlight);
+        
+        if (activeHighlights.Length>0)
         {
-            highlights[i] = highlightPrefab.HighlightTarget(targets[i]);
+            foreach (var h in activeHighlights)
+            {
+                h.DestroyHighlight();
+            }
         }
+
+        if (highlight != null)
+        {
+            var targets = GetComponentsInChildren<Hilightable>();
+        
+            activeHighlights = new Highlight[targets.Length];
+            for (int i = 0; i < targets.Length; i++)
+            {
+                activeHighlights[i] = highlight.HighlightTarget(targets[i]);
+            }
+        }
+        else
+        {
+            activeHighlights = Array.Empty<Highlight>();
+        }
+
     }
+    
 }
