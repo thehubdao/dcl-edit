9983ae1d47f35d2113ec9b08e8af19d8753f69e8
cblech <cblech@ymail.com>
2021-12-10 01:42:00 +0100

    Updated version number to 0.2.1

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -8,7 +8,7 @@ function getBinary(){
 
     if (type !== 'Windows_NT') throw new Error(`Unsupported platform: Currently only for Windows`);
 
-    return new Binary("dcl-edit.exe","https://github.com/cblech/dcl-edit/releases/download/0.2.0/dcl-edit-0.2.0-windows-x86.tar.gz")
+    return new Binary("dcl-edit.exe","https://github.com/cblech/dcl-edit/releases/download/0.2.1/dcl-edit-0.2.1-windows-x86.tar.gz")
 }
 
 module.exports = getBinary;
