ab87f93c7d0d839d495974d727109aa3aafe6a69
cblech <cblech@users.noreply.github.com>
2022-03-02 19:08:17 +0100

    Fixed Bug: Only highlighting first sub mesh

--- Assets/Highlight/Scripts/Highlight.cs
+++ Assets/Highlight/Scripts/Highlight.cs
@@ -5,29 +5,43 @@ using System.Linq;
 
 public class Highlight : MonoBehaviour
 {
-	//Settings
-	[SerializeField]
-	private List<MeshFilter> meshFilters = default;
-	[SerializeField]
+    //Settings
+    [SerializeField]
+    private List<MeshFilter> meshFilters = default;
+    [SerializeField]
 
 
-	public Highlight HighlightTarget(Hilightable target)
-	{
-		Highlight instance = Instantiate(gameObject, target.transform).GetComponent<Highlight>();
+    public Highlight HighlightTarget(Hilightable target)
+    {
+        Highlight instance = Instantiate(gameObject, target.transform).GetComponent<Highlight>();
 
-		Mesh targetMesh = target.OwnMeshFilter.sharedMesh;
-		foreach (MeshFilter meshFilter in instance.meshFilters)
-		{
-			meshFilter.mesh = targetMesh;
-		}
+        Mesh targetMesh = target.OwnMeshFilter.sharedMesh;
+        foreach (MeshFilter meshFilter in instance.meshFilters)
+        {
+            meshFilter.mesh = targetMesh;
 
-		return instance;
-	}
+            // If multiple sub meshes exist, add one Material per sub mesh
+            if (meshFilter.mesh.subMeshCount > 1)
+            {
+                var mat = meshFilter.GetComponent<MeshRenderer>().materials[0];
+                var mats = new List<Material>();
+
+                for (int i = 0; i < meshFilter.mesh.subMeshCount; i++)
+                {
+                    mats.Add(mat);
+                }
+
+                meshFilter.GetComponent<MeshRenderer>().materials = mats.ToArray();
+            }
+        }
+
+        return instance;
+    }
 
 
-	public void DestroyHighlight()
-	{
-		if(gameObject!=null && gameObject)
-		    Destroy(gameObject);
-	}
+    public void DestroyHighlight()
+    {
+        if (gameObject != null && gameObject)
+            Destroy(gameObject);
+    }
 }
