a62c1754d6e4278cb72acb4708dbb5bbd10e1e65
SuperSexySintoras <83906287+SuperSexySintoras@users.noreply.github.com>
2022-03-17 11:59:51 +0100

    Added Undo

--- Assets/Scripts/Systems/DropToGround.cs
+++ Assets/Scripts/Systems/DropToGround.cs
@@ -5,19 +5,25 @@ using UnityEngine.EventSystems;
 
 public class DropToGround : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    static TransformUndo dropWithPhysicsUndo;
 
-    public static void Drop()
+    public static void Drop()//Drop an item to the ground by making a sweeptest and finding the distance untill the first collision
     {
         if (DclSceneManager.PrimarySelectedEntity != null)
         {
+            TransformUndo transformUndo = new TransformUndo(DclSceneManager.AllSelectedEntities);
+            transformUndo.SaveBeginningState();
+
             var primaryEntity = DclSceneManager.PrimarySelectedEntity;
-            float collisionCheckDistance = 256;
+            float collisionCheckDistance = 256;//max distance for check
+
             Rigidbody rb = primaryEntity.GetComponent(typeof(Rigidbody)) as Rigidbody;
             if (rb == null)
             {
                 rb = primaryEntity.gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
             }
-            MeshCollider mesh= primaryEntity.gameObject.AddComponent(typeof(MeshCollider))as MeshCollider;
+
+            MeshCollider mesh = primaryEntity.gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
             mesh.convex = true;
             RaycastHit hit;
             if (rb.SweepTest(new Vector3(0, -1, 0), out hit, collisionCheckDistance))//this only works with simple or convex meshes
@@ -26,33 +32,47 @@ public class DropToGround : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
             }
             Destroy(primaryEntity.GetComponent(typeof(Rigidbody)));
             Destroy(primaryEntity.GetComponent(typeof(MeshCollider)));
+            DclSceneManager.OnSelectedEntityTransformChange.Invoke();
+
+            transformUndo.SaveEndingState();
+            transformUndo.AddUndoItem();
 
         }
     }
 
-    public void OnPointerDown(PointerEventData eventData)//make button hold and release instead of toggle//tool visuals//shortcut G STRG+G//systems view 3d input system
+    public void OnPointerDown(PointerEventData eventData)
     {
+
         DropWithPhysics();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+
         RemoveRigidBody();
     }
 
     public static void RemoveRigidBody()
     {
+        DclSceneManager.OnSelectedEntityTransformChange.Invoke();
         var primaryEntity = DclSceneManager.PrimarySelectedEntity;
         Destroy(primaryEntity.GetComponent(typeof(Rigidbody)));
+        dropWithPhysicsUndo.SaveEndingState();
+        dropWithPhysicsUndo.AddUndoItem();
     }
 
-    public static void DropWithPhysics()
+    public static void DropWithPhysics()//Drop an item towards the ground by adding a Rigidbody
     {
         if (DclSceneManager.PrimarySelectedEntity != null)
         {
+            dropWithPhysicsUndo = new TransformUndo(DclSceneManager.AllSelectedEntities);
+            dropWithPhysicsUndo.SaveBeginningState();
+
             var primaryEntity = DclSceneManager.PrimarySelectedEntity;
 
             primaryEntity.gameObject.AddComponent(typeof(Rigidbody));
+
         }
     }
 }
+
