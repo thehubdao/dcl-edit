47fdf5dc74d46eae6bf548a99de994e417aa6c91
cblech <cblech@users.noreply.github.com>
2022-07-30 17:53:52 +0200

    Fixed help showing unused argument

--- npm/run.js
+++ npm/run.js
@@ -3,7 +3,7 @@
 let args = require("yargs")(process.argv.slice(2))
     .usage("Usage: dcl-edit [options]")
     .help("h").alias("h", "help").describe("h", "Show this help")
-    .alias("p", "projectPath").describe("p", "Set the decentraland project path. This is the path, where also the scene.json file lives. Defaults to current working directory")
+    //.alias("p", "projectPath").describe("p", "Set the decentraland project path. This is the path, where also the scene.json file lives. Defaults to current working directory")
     .argv;
 
 let projectDir = process.cwd()
