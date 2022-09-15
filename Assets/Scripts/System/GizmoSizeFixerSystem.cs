89ea3568a8b7e0c3c6de911b30d4a1c41a1ac490
cblech <cblech@users.noreply.github.com>
2022-09-15 06:29:23 +0200

    Make gizmo size more consistent

--- Assets/Scripts/System/GizmoSizeFixerSystem.cs
+++ Assets/Scripts/System/GizmoSizeFixerSystem.cs
@@ -9,12 +9,24 @@ public class GizmoSizeFixerSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var dist = Vector3.Distance(transform.position, EditorStates.CurrentCameraState.Position);
+        // vector from camera to gizmo
+        var diffVector = transform.position - EditorStates.CurrentCameraState.Position;
 
+        // camera forward vector
+        var camForward = EditorStates.CurrentCameraState.Forward;
+
+        // project diffVector onto forward vector
+        var proj = Vector3.Project(diffVector, camForward);
+
+        // the length of the projected vector is the perceived distance to the gizmo
+        var dist = proj.magnitude;
+
+        // some artificial multiplier to give the gizmo a nice size
         var multiplier = 0.2f; // TODO: gizmo size settings
 
         dist *= multiplier;
 
+        // set the scale of the gizmo
         transform.localScale = new Vector3(dist,dist,dist);
     }
 }
