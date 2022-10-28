44861e28128f52c4c7670be9381074f4ad666698
cblech <cblech@users.noreply.github.com>
2022-10-28 18:56:10 +0200

    Add Testing

--- Assets/Scripts/Tests/EditModeTests/LoadSaveSceneV2Test.cs
+++ Assets/Scripts/Tests/EditModeTests/LoadSaveSceneV2Test.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Linq;
 using Assets.Scripts.EditorState;
+using Assets.Scripts.SceneState;
 using Assets.Scripts.System;
 using Assets.Scripts.Tests.EditModeTests.TestUtility;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
-using UnityEditor;
 using UnityEngine;
 
 namespace Assets.Scripts.Tests.EditModeTests
@@ -16,13 +15,13 @@ namespace Assets.Scripts.Tests.EditModeTests
         public void LoadScene()
         {
             var pathState = new MockPathState("simple-load-test");
-            var lsSystem = new SceneLoadSaveSystem(pathState);
+            var loadSaveSystem = new SceneLoadSaveSystem(pathState);
             var sceneDirectoryState = new SceneDirectoryState();
             var scenePath = pathState.ProjectPath + "/dcl-edit/saves/v2/New Scene.dclscene";
 
             sceneDirectoryState.DirectoryPath = scenePath;
 
-            lsSystem.Load(sceneDirectoryState);
+            loadSaveSystem.Load(sceneDirectoryState);
 
             var scene = sceneDirectoryState.CurrentScene;
 
@@ -58,6 +57,69 @@ namespace Assets.Scripts.Tests.EditModeTests
             Assert.NotNull(cubeEntBoxShape);
 
             Assert.AreEqual("Shape", cubeEntBoxShape.NameOfSlot);
+        }
+
+        [Test]
+        public void LoadedFilePaths()
+        {
+            var pathState = new MockPathState("simple-load-test", true);
+            var loadSaveSystem = new SceneLoadSaveSystem(pathState);
+            var scenePath = pathState.ProjectPath + "/dcl-edit/saves/v2/New Scene.dclscene";
+
+            // Load scene, add a entity, save, remove a entity, and save again. This should result in the original scene
+            {
+                var sceneDirectoryState = new SceneDirectoryState
+                {
+                    DirectoryPath = scenePath
+                };
+
+                loadSaveSystem.Load(sceneDirectoryState);
+
+                var scene = sceneDirectoryState.CurrentScene;
+
+                Assert.NotNull(scene);
+
+                var cubeEnt = scene.GetEntityById(Guid.Parse("733338c0-c576-4465-975b-248e9e5e7b63"));
+                Assert.NotNull(cubeEnt);
+
+                var sphereEnt = scene.GetEntityById(Guid.Parse("84089323-69df-4d27-b0da-a51afcdd88bf"));
+                Assert.NotNull(sphereEnt);
+
+                // Add entity without command. TODO: Should be done differently
+                var newEntityId = Guid.Parse("12345678-1234-1234-1234-123456789abc");
+                scene.AddEntity(new DclEntity(newEntityId, "New Entity"));
+
+                var newEntity = scene.GetEntityById(newEntityId);
+                Assert.NotNull(newEntity);
+
+                loadSaveSystem.Save(sceneDirectoryState);
+
+                // Delete entity without command. TODO: Should be done differently
+                scene.RemoveEntity(newEntityId);
+
+                newEntity = scene.GetEntityById(newEntityId);
+                Assert.Null(newEntity);
+
+                loadSaveSystem.Save(sceneDirectoryState);
+            }
+
+            // Load scene again and observe, that there are only two entities
+            {
+                var sceneDirectoryState = new SceneDirectoryState
+                {
+                    DirectoryPath = scenePath
+                };
+
+                loadSaveSystem.Load(sceneDirectoryState);
+
+                var scene = sceneDirectoryState.CurrentScene;
+
+                Assert.NotNull(scene);
+
+                var newEntityId = Guid.Parse("12345678-1234-1234-1234-123456789abc");
+                var newEntity = scene.GetEntityById(newEntityId);
+                Assert.Null(newEntity);
+            }
         }
     }
 }
--- Assets/Scripts/Tests/EditModeTests/TestUtility/MockPathState.cs
+++ Assets/Scripts/Tests/EditModeTests/TestUtility/MockPathState.cs
@@ -1,15 +1,51 @@
+using System.IO;
 using Assets.Scripts.EditorState;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Assets.Scripts.Tests.EditModeTests.TestUtility
 {
     public class MockPathState : IPathState
     {
-        public MockPathState(string testProject)
+        public MockPathState(string testProject, bool makeTemporaryProject = false)
         {
             ProjectPath = Application.dataPath + "/../../dcl-edit-testing/projects/" + testProject;
+
+            if (makeTemporaryProject)
+            {
+                MakeTemporaryCopyOfProject();
+            }
         }
 
-        public string ProjectPath { get; }
+        public string ProjectPath { get; private set; }
+
+        /**
+         * Changes ProjectPath to a new temporary folder and copies the content from the old ProjectPath to the new folder.
+         * Use this, when a test requires a change in the project, like saving a scene
+         */
+        public void MakeTemporaryCopyOfProject()
+        {
+            var newProjectPath = Application.temporaryCachePath + "/tmp_project_" + Random.Range(int.MinValue, int.MaxValue);
+
+            Directory.CreateDirectory(newProjectPath);
+            CopyFilesRecursively(ProjectPath, newProjectPath);
+
+            ProjectPath = newProjectPath;
+        }
+
+        private static void CopyFilesRecursively(string sourcePath, string targetPath)
+        {
+            //Now Create all of the directories
+            foreach (var dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (var newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+            }
+        }
     }
 }
