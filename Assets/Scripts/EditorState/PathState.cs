1cbe36ea037cb5b33cbffe8932279d8362d9ec77
cblech <cblech@users.noreply.github.com>
2022-11-15 05:26:03 +0100

    Added Builder asset loader

--- Assets/Scripts/EditorState/PathState.cs
+++ Assets/Scripts/EditorState/PathState.cs
@@ -38,4 +38,6 @@ public class PathState
             return _projectPath;
         }
     }
+
+    public string CachePath = null;
 }
---
+++ Assets/Scripts/System/WebRequestSystem.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.System
+{
+    public class WebRequestSystem
+    {
+        public void Get(string url, Action<UnityWebRequestAsyncOperation> then)
+        {
+            void OnRequestComplete(AsyncOperation operation)
+            {
+                var request = operation as UnityWebRequestAsyncOperation;
+
+                if (request == null)
+                {
+                    Debug.LogError("request is null");
+                    return;
+                }
+
+                then(request);
+            }
+
+            var request = UnityWebRequest.Get(url).SendWebRequest();
+            if (request.isDone)
+            {
+                OnRequestComplete(request);
+            }
+            else
+            {
+                request.completed += OnRequestComplete;
+            }
+        }
+    }
+}
