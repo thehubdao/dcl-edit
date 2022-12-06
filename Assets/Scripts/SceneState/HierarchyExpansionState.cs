5881dcb4552fb0655a8214f7c7233ac68bc89e4a
OswaldUnity <oswald@thedac.info>
2022-12-06 17:34:16 -0500

    Fix property name conventions

--- Assets/Scripts/SceneState/HierarchyExpansionState.cs
+++ Assets/Scripts/SceneState/HierarchyExpansionState.cs
@@ -8,8 +8,8 @@ namespace Assets.Scripts.SceneState
     {
         private Dictionary<Guid, bool> _expandedEntities = new Dictionary<Guid, bool>();
         
-        private Dictionary<Guid, bool> _prevExpandedState;
-        private Guid _prevSelectedEntity = Guid.Empty;
+        private Dictionary<Guid, bool> prevExpandedState;
+        private Guid prevSelectedEntity = Guid.Empty;
         
         private const bool _defaultExpansionState = false;
 
@@ -48,37 +48,37 @@ namespace Assets.Scripts.SceneState
             if (_expandedEntities.TryGetValue(id, out var currentValue))
             {
                 _expandedEntities[id] = !currentValue;
-                if (fromUserInteraction && _prevExpandedState != null)
-                    _prevExpandedState[id] = !currentValue;
+                if (fromUserInteraction && prevExpandedState != null)
+                    prevExpandedState[id] = !currentValue;
                 return;
             }
 
             // else
             _expandedEntities.Add(id, !_defaultExpansionState);
-            if(fromUserInteraction && _prevExpandedState != null)
-                _prevExpandedState.Add(id, !_defaultExpansionState);
+            if(fromUserInteraction && prevExpandedState != null)
+                prevExpandedState.Add(id, !_defaultExpansionState);
         }
 
         public void ExpandParents(Guid selectedEntity, IEnumerable<Guid> parentList)
         {
             // Same entity selected
-            if (selectedEntity == _prevSelectedEntity) return;
+            if (selectedEntity == prevSelectedEntity) return;
             
             // Deselected entity - Reset
             if (selectedEntity == Guid.Empty)
             {
-                _expandedEntities = _prevExpandedState;
-                _prevSelectedEntity = Guid.Empty;
-                _prevExpandedState = null;
+                _expandedEntities = prevExpandedState;
+                prevSelectedEntity = Guid.Empty;
+                prevExpandedState = null;
                 return;
             }
             
             // New entity selected
-            if (_prevExpandedState != null)
-                _expandedEntities = _prevExpandedState;
+            if (prevExpandedState != null)
+                _expandedEntities = prevExpandedState;
             
-            _prevSelectedEntity = selectedEntity;
-            _prevExpandedState = new Dictionary<Guid, bool>(_expandedEntities);
+            prevSelectedEntity = selectedEntity;
+            prevExpandedState = new Dictionary<Guid, bool>(_expandedEntities);
 
             foreach (var parent in parentList.Where(parent => !IsExpanded(parent)))
             {
