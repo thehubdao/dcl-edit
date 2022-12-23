31525fb1d89d2f0502ea33f8c9f87b9d51ba19a4
Alonso <cardenasjairods@gmail.com>
2022-12-23 11:05:41 -0500

    CU-861m41y5d Injecting gizmo state dependency and modifiying the current mode on button click. Left to check why the InvokeSelectionChangedEvent when CurrentMode Set unselects the entity.

---
+++ Assets/Scripts/Interaction/GizmoModeInteraction.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.EditorState;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Interaction
+{
+    public class GizmoModeInteraction : MonoBehaviour
+    {
+        private GizmoState _gizmoState;
+        
+        [Inject]
+        private void Construct(GizmoState gizmoState)
+        {
+            _gizmoState = gizmoState;
+        }
+        public void SetManipulatorTranslate()
+        {
+            _gizmoState.CurrentMode = GizmoState.Mode.Translate;
+        }
+        public void SetManipulatorRotate()
+        {
+            _gizmoState.CurrentMode = GizmoState.Mode.Rotate;
+        }
+        public void SetManipulatorScale()
+        {
+            _gizmoState.CurrentMode = GizmoState.Mode.Scale;
+        }
+    }
+}
