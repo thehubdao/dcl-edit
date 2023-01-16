deaf770c51129fb5b69dfa9189f18ac5754648b6
MGHStephan <stephan.kingsbery@thedac.info>
2023-01-17 00:45:28 +0100

    Added spacer prefab & handler
    Cleaned up spacer prefab
    
    Fixed spacer prefab

---
+++ Assets/Scripts/Visuals/UiHandler/SpacerHandler.cs
@@ -0,0 +1,11 @@
+using Assets.Scripts.Visuals.UiHandler;
+using UnityEngine;
+
+namespace Visuals.UiHandler
+{
+    public class SpacerHandler: MonoBehaviour
+    {
+        [SerializeField]
+        public RightClickHandler rightClickHandler;
+    }
+}
