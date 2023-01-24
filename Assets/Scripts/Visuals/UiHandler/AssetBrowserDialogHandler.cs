a3e3f1598390ad03fc9acf04f2779b530ac2ea6e
j-eissler <57557568+j-eissler@users.noreply.github.com>
2023-01-24 14:08:57 +0100

    Add asset browser dialog prefab

---
+++ Assets/Scripts/Visuals/UiHandler/AssetBrowserDialogHandler.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Visuals.UiHandler
+{
+    public class AssetBrowserDialogHandler : MonoBehaviour
+    {
+        [SerializeField]
+        public Guid entityId;
+    }
+}
