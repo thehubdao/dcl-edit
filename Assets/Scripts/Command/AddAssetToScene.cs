bcff8518927aab6f5b158a449d469d80b484e90d
j-eissler <57557568+j-eissler@users.noreply.github.com>
2023-02-20 22:54:02 +0100

    Update selection state when Do() or Undo() is called on AddAssetToScene commands

--- Assets/Scripts/Command/AddAssetToScene.cs
+++ Assets/Scripts/Command/AddAssetToScene.cs
@@ -2,6 +2,7 @@ using Assets.Scripts.Command.Utility;
 using Assets.Scripts.Events;
 using Assets.Scripts.SceneState;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Command
@@ -12,6 +13,7 @@ namespace Assets.Scripts.Command
         protected string entityCustomName;
         protected Guid assetId;
         protected Vector3 positionInScene;
+        protected SelectionUtility.SelectionWrapper oldSelection;
 
         public AddAssetToScene(Guid entityId, string entityCustomName, Guid assetId, Vector3 positionInScene)
         {
@@ -25,6 +27,7 @@ namespace Assets.Scripts.Command
         {
             EntityUtility.DeleteEntity(sceneState, entityId);
             editorEvents.InvokeHierarchyChangedEvent();
+            RestoreOldSelection(sceneState, editorEvents);
         }
 
         protected void AddTransformComponent(DclEntity entity)
@@ -35,8 +38,30 @@ namespace Assets.Scripts.Command
 
         protected void SelectEntity(DclScene sceneState, EditorEvents editorEvents, DclEntity entity)
         {
+            StoreOldSelection(sceneState);
             sceneState.SelectionState.SecondarySelectedEntities.Clear();
             sceneState.SelectionState.PrimarySelectedEntity = entity;
+            editorEvents.InvokeSelectionChangedEvent();
+        }
+
+        private void StoreOldSelection(DclScene sceneState)
+        {
+            Guid primaryId = sceneState.SelectionState.PrimarySelectedEntity?.Id ?? Guid.Empty;
+            List<Guid> secondaryIds = new List<Guid>();
+            foreach (DclEntity entity in sceneState.SelectionState.SecondarySelectedEntities)
+            {
+                secondaryIds.Add(entity.Id);
+            }
+            oldSelection = new SelectionUtility.SelectionWrapper(primaryId, secondaryIds);
+        }
+
+        private void RestoreOldSelection(DclScene sceneState, EditorEvents editorEvents)
+        {
+            sceneState.SelectionState.PrimarySelectedEntity = sceneState.GetEntityById(oldSelection.Primary);
+            foreach (Guid id in oldSelection.Secondary)
+            {
+                sceneState.SelectionState.SecondarySelectedEntities.Add(sceneState.GetEntityById(id));
+            }
             editorEvents.InvokeSelectionChangedEvent();
         }
     }
