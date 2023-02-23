ebb8f4a38c0c1407d214f177dbdcf3bf70863d13
j-eissler <57557568+j-eissler@users.noreply.github.com>
2023-02-23 16:28:39 +0100

    Remove decimal separator chars if there is more than one

--- Assets/Scripts/System/NumberInputSystem.cs
+++ Assets/Scripts/System/NumberInputSystem.cs
@@ -8,12 +8,36 @@ namespace Assets.Scripts.System
         public float? ValidateNumberInput(string value)
         {
             value = value.Replace(',', '.');
+            value = RemoveExcessDecimalSeparators(value);
             if (float.TryParse(value, Filter, CultureInfo.InvariantCulture, out var result))
             {
                 return result;
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Checks if there is more than one decimal separator (dot) and removes the excess ones.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        string RemoveExcessDecimalSeparators(string value)
+        {
+            int decimalPointCount = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] == '.')
+                {
+                    decimalPointCount++;
+                    if (decimalPointCount > 1)
+                    {
+                        value = value.Remove(i, 1);
+                    }
+                }
+            }
+
+            return value;
         }
     }
 }
