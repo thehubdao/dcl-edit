6ede7a8761eeebd16900fbe8dac06bac2d7a690c
j-eissler <57557568+j-eissler@users.noreply.github.com>
2023-03-03 15:25:53 +0100

    Add scene filter to asset browser

--- Assets/Scripts/Visuals/UiAssetBrowserVisuals.cs
+++ Assets/Scripts/Visuals/UiAssetBrowserVisuals.cs
@@ -130,6 +130,7 @@ namespace Assets.Scripts.Visuals
                 {
                     new ContextMenuTextItem("Add model filter", () => assetBrowserSystem.AddFilter(AssetMetadata.AssetType.Model)),
                     //new ContextMenuTextItem("Add image filter", () => assetBrowserSystem.AddFilter(AssetMetadata.AssetType.Image)),
+                    new ContextMenuTextItem("Add scene filter", () => assetBrowserSystem.AddFilter(AssetMetadata.AssetType.Scene)),
                     new ContextMenuTextItem("Sort by name (A-Z)", () => assetBrowserSystem.ChangeSorting(AssetBrowserState.Sorting.NameAscending)),
                     new ContextMenuTextItem("Sort by name (Z-A)", () => assetBrowserSystem.ChangeSorting(AssetBrowserState.Sorting.NameDescending)),
                 });
