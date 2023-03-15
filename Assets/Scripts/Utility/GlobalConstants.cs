a26f3a5f626bd0e837e0a86ea8847e5f5e6b59c2
OswaldUnity <oswald@thedac.info>
2023-03-15 18:20:54 -0500

    Added GlobalConstants file This is needed in order to avoid changing multiple places instead of just one

---
+++ Assets/Scripts/Utility/GlobalConstants.cs
@@ -0,0 +1,16 @@
+namespace Assets.Scripts.Utility
+{
+    public static class GlobalConstants
+    {
+        public struct V1Paths
+        {
+            public const string save = "/dcl-edit/saves/save.json";
+            public const string assets = "/dcl-edit/saves/assets.json";
+        }
+
+        public struct Extensions
+        {
+            public const string dclAsset = ".dclasset";
+        }
+    }
+}
