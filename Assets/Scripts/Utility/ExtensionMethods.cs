165a755c0e44be5eb43f5ea7250b775599dc05a6
OswaldUnity <oswald@thedac.info>
2023-06-21 11:39:22 -0500

    Added ExtensionMethod for adding range on dictionary

---
+++ Assets/Scripts/Utility/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Utility
+{
+    public static class ExtensionMethods
+    {
+        public static void AddRange<TKey, TValue>(this Dictionary<TKey, TValue> source, Dictionary<TKey, TValue> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("Collection is null");
+            }
+
+            foreach (var item in collection)
+            {
+                source[item.Key] = item.Value;
+            } 
+        }
+    }
+}
